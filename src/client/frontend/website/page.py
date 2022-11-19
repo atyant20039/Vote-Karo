71e053376d663274011ada49705de50c05268e1b
Atyant Sony <atyant20039@iiitd.ac.in>
2022-11-19 19:20:52 +0530

    minor changes

--- src/client/frontend/website/page.py
+++ src/client/frontend/website/page.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template
 
 page = Blueprint('page', __name__)
 
-@page.route('/')
-def home():
+@page.route('/', methods=['GET', 'POST'])
+def webpage():
+    if request.method == 'POST':
+        voterid = request.form.get('voterid')
+        choice = request.form.get('radiolist')
     return render_template("webpage.html")
